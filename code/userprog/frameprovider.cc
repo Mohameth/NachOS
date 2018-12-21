6bf48290c296c129600a7312f3410c5a7bd6b4d7
FirmYn <jeremy.3y@gmail.com>
2018-12-21 16:11:57 +0100

    VACANCES....

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -4,6 +4,25 @@
 #include "system.h"
 #include "frameprovider.h"
 
+FrameProvider::FrameProvider(int numPage, TranslationEntry * entry) {
+    frameBitMap = new BitMap(numPage);
+    entries = entry;
+    numPage = numPages;
 
+    for (int i = 0; i < numPages; i++) {
+        if (entries[i].valid)
+            frameBitMap->Mark(i);
+    }
+}
+
+FrameProvider::~FrameProvider() {
+    delete frameBitMap;    
+}
+
+FrameProvider::GetEmptyFrame() {
+    int freeFrame = frameBitMap->Find();
+    TranslationEntry * freeEntry = entries[i];
+    
+}
 
 #endif // CHANGED
--- code/userprog/frameprovider.h
+++ code/userprog/frameprovider.h
@@ -5,14 +5,24 @@
 
 #include "copyright.h"
 #include "utility.h"
+#include "bitmap.h"
 
 class FrameProvider {
     public:
-        FrameProvider();
-
+        FrameProvider(int numPage, TranslationEntry * entries);
+        
         ~FrameProvider();        //clean up
 
+        TranslationEntry * GetEmptyFrame();
+
+        void ReleaseFrame(TranslationEntry * pageTable);
+
+        int NumAvailFrame();
     private:
+        Semaphore * mutex;
+        BitMap * frameBitMap;
+        TranslationEntry * entries;
+        int numPages;
 
 };
 

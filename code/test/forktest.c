2bfa5cd335c1dec3af6e4be19135c4c48da43e6f
FirmYn <jeremy.3y@gmail.com>
2019-01-10 14:02:03 +0100

    Commit avant essai autre technique

--- code/test/forktest.c
+++ code/test/forktest.c
@@ -6,6 +6,16 @@ int main()
     ForkExec("putchar");
     // ForkExec("userpages1");
     PutChar('L');
+    PutChar('O');
+    PutChar('L');
+    PutChar('\n');
+    PutChar('L');
+    PutChar('O');
+    PutChar('L');
+    PutChar('\n');
+    PutChar('L');
+    PutChar('O');
+    PutChar('L');
     PutChar('\n');
     while(1);
 }
